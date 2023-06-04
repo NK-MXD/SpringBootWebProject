44fcb9c44d4a6b5d2af6050a14b4dfafe3392c18
lsyszbd88 <1641080825@qq.com>
2023-06-04 10:07:17 +0800

    成功报名

--- exam-vue/src/views/application/components/Account.vue
+++ exam-vue/src/views/application/components/Account.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <el-form>
     <el-form-item label="真实姓名">
       <el-input v-model.trim="user.realName" readonly />
@@ -19,6 +20,13 @@
       <el-button type="primary" @click="submit">报名</el-button>
     </el-form-item>
   </el-form>
+  <el-dialog :visible="showModal" @close="closeModal" width="50%">
+      <div class="popup-content">
+        <div class="popup-text">请扫描二维码，进行支付</div>
+        <img :src="imageUrl" alt="弹出的图片" class="popup-image">
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -44,6 +52,12 @@ export default {
     },
     // dialogVisible: false,
   },
+  data() {
+    return {
+      showModal: false, // 控制弹窗显示
+      imageUrl: 'https://image-1305894911.cos.ap-beijing.myqcloud.com/Obsidian/202306040958010.jpg' // 图片 URL
+    }
+  },
   created() {
     this.fetchUserData();
   },
@@ -61,11 +75,18 @@ export default {
           type: 'success',
           duration: 2000
         })
-        // this.departId = ''
-        // this.logout()
-        // this.dialogVisible = true
+        this.showModal = true; // 显示弹窗
       })
-      this.dialogVisible = true
+    },
+    closeModal() {
+      this.showModal = false; // 关闭弹窗
+      this.$notify({
+          title: '成功',
+          message: '报名成功！！',
+          type: 'success',
+          duration: 2000
+        })
+        this.showModal = false; // 显示弹窗
     },
     fetchUserData() {
       const data = {
@@ -103,3 +124,20 @@ export default {
   }
 }
 </script>
+
+<style>
+.popup-content {
+  text-align: center;
+}
+
+.popup-text {
+  font-size: 24px;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.popup-image {
+  width: 50%; /* 设置图片的宽度 */
+  height: auto; /* 自动调整高度以保持原始比例 */
+}
+</style>

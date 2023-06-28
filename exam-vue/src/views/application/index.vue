c508f3c2d92c37951c6ef1e8b3a71da7d3e879af
@civilizwa <2243987073@qq.com>
2023-06-28 21:42:06 +0800

    添加应用索引

---
+++ exam-vue/src/views/application/index.vue
@@ -0,0 +1,66 @@
+﻿<template>
+  <div class="app-container">
+    <div v-if="user">
+      <el-row :gutter="20">
+
+        <el-col :span="6" :xs="24">
+          <user-card :user="user" />
+        </el-col>
+
+        <el-col :span="18" :xs="24">
+          <el-card>
+            <el-tabs v-model="activeTab">
+              <el-tab-pane label="考试报名" name="account">
+                <account :user="user" />
+              </el-tab-pane>
+            </el-tabs>
+          </el-card>
+        </el-col>
+
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+// import UserCard from './components/UserCard'
+// import Account from './components/Account'
+
+export default {
+  name: 'Profile',
+  components: { UserCard, Account },
+  data() {
+    return {
+      user: {},
+      activeTab: 'account'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name',
+      'avatar',
+      'roles',
+      'departId',
+      'phone',
+      'chnid'
+    ])
+  },
+  created() {
+    this.getUser()
+  },
+  methods: {
+    getUser() {
+      this.user = {
+        name: this.name,
+        role: this.roles.join(' | '),
+        departId: this.departId,
+        phone: this.phone,
+        chnid: this.chnid,
+        email: 'admin@test.com',
+        avatar: this.avatar
+      }
+    }
+  }
+}
+</script>
